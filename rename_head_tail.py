b9bf31e0aa923a29f112bc418482070ec67cdbc6
mmangione <martina.mangione-cic-it@ibm.com>
2020-06-23 01:45:27 +0200

    add wordnet converter

--- rename_head_tail.py
+++ rename_head_tail.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from nltk.corpus import wordnet as wn
 
 # OBIETTIVO: Dato un dataframe per FB15k e WN18 recupera i nodi di ogni nodo:
 
@@ -24,10 +25,26 @@ def recover_name_fb15k(df):
     df['head_name'] = heads
     df['tail_name'] = tails
 
+def recover_name_wordnet(df):
+    columns = list(df)
+    heads = []
+    tails = []
+    for i in columns:
+        head = df[i][0]
+        tail = df[i][2]
+        # Da un synset è possibile ricavare una lista di lemmi ossia sinonimi per quella parola. Di regola prenderemo sempre il primo
+        head = wn.synset_from_pos_and_offset('n', head)._lemma_names[0]
+        tail = wn.synset_from_pos_and_offset('n',tail)._lemma_names[0]
+        heads.append(head)
+        tails.append(tail)
+    df['head_name'] = heads
+    df['tail_name'] = tails
 
 
 # IL FILE DI INPUT SARA' QUELLO GENERATO DA estraggo_righe_comuni.py. Finito estraggo_righe_comuni.py va fatta la sostituzione qui
-df=pd.read_csv('data/FB15k/ComplEx/complex_filtered_ranks.csv',sep=';',names=['head','relation','tail','head_rank','tail_rank'])
-recover_name_fb15k(df)
+#df=pd.read_csv('data/FB15k/ComplEx/complex_filtered_ranks.csv',sep=';',names=['head','relation','tail','head_rank','tail_rank'])
+#recover_name_fb15k(df)
 
-#STESSA COSA ANDRA' FATTA PER WN18
+#TEST PER WN18
+df=pd.read_csv('data/WN18/ComplEx/complex_filtered_ranks.csv',sep=';',names=['head','relation','tail','head_rank','tail_rank'])
+recover_name_wordnet(df)
